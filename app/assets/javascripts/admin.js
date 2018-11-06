16673f32f1a5934b23bca5cc44d3d6f1780affb7
Shumai <shumai9@github.com>
2018-11-06 05:31:19 +0000

    style fixed

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,3 +5,38 @@
 function jsRoar(name) {
   alert('I am' + name +'. Hear me roar');
 }
+
+// var blogster = new Object();
+// // setting attributies for the object
+// blogster.owner = "Shumai";
+// blogster.content = "Articlezila";
+// blogster.color = "Bulebastic";
+
+// hash format
+var blogster = {
+  owner: "Shumai",
+  content: "Articlezila",
+  color: "Bulebastic"
+}
+
+console.log(blogster.owner);
+
+// as format var multiarticle = new blogster();
+//object constuctor is like class creator
+function Blogster(owner,content,color){
+  this.owner = owner;
+  this.content = content;
+  this.color = color;
+}
+
+//let write clousers
+function blogista(){
+  var title = "Bloga";
+  var content = "Bodygar";
+
+  function blogbuster(){
+    var article =title + content;
+    return article;
+  }
+return blogbuster;
+}
